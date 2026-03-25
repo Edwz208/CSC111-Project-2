88184110755bfb2decd2fd2c51b0c53d71e399b6
Edwin Z <147886945+Edwz208@users.noreply.github.com>
2026-03-25 18:55:41 -0400

    Refactor data sanitization functions and add new features
    Refactor data sanitization functions to improve structure and add new functionality.

--- data_sanitization.py
+++ data_sanitization.py
@@ -2,34 +2,40 @@ from __future__ import annotations
 import csv
 from typing import Any
 import ast
+import pandas as pd
 
 
-def clean_anime(anime_file) -> list[dict[str, Any]]:
+def clean_anime(anime_file: str) -> list[dict[str, Any]]:
     """
     Return a list of anime, with each one being a dictionary mapping of its attributes
-    Currently does not sanitize, just returns list
+    Currently does not other than ensuring that title and id pairings are unique.
+    IMPORTANT: DOES NOT WORK RIGHT NOW DUE TO SYNPSOSIS STRETCHING BEYOND A SINGLE LINE
     """
-    anime_list = []
-    anime_ids = set()
-    with open(anime_file, mode='r', newline='') as file:
-        reader = csv.reader(file)
-        next(reader)
-        for row in reader:
-            if row[0] not in anime_ids:
-                anime_list.append({"id": row[0], "title": row[1], "synopsis": row[2],"genre": row[3],
-                                   "aired": row[4], "episodes": row[5], "members": row[6], "popularity": row[7],
-                                   "ranked": row[8], "score": row[9]})
-    return anime_list
+    # anime_list = []
+    # anime_title_and_id = set()
+    # with open(anime_file, mode='r', newline='') as file:
+    #     reader = csv.reader(file)
+    #     next(reader)
+    #     for row in reader:
+    #         anime_id = row[0]
+    #         title = row[1]
+    #         if (anime_id, title) not in anime_title_and_id:
+    #             anime_title_and_id.add((anime_id, title))
+    #             anime_list.append({"id": anime_id, "title": title, "synopsis": row[2], "genre": row[3],
+    #                                "aired": row[4], "episodes": row[5], "members": row[6], "popularity": row[7],
+    #                                "ranked": row[8], "score": row[9]})
+    # return anime_list
+    raise NotImplementedError
 
 
-def clean_profiles(profiles_file) -> list[dict[str, Any]]:
+def clean_profiles(profiles_file: str) -> dict[str, list[int]]:
     """
-    Return a list of user profiles, with each one being a dictionary mapping of its attributes
+    Return a mapping of user profiles to their favourite_anime
     Gets rid of duplicate user profiles, duplicate anime ids within a favourite anime list, and skips users entirely
     if the user has invalid anime id.
     """
-    user_list = []
-    user_ids = set()
+    user_map = {}
+    USER_MINIMUM_FAVOURITES = 2
     with open(profiles_file, mode='r', newline='') as file:
         reader = csv.reader(file)
         next(reader)
@@ -46,11 +52,44 @@ def clean_profiles(profiles_file) -> list[dict[str, Any]]:
                 except ValueError:
                     favourites_clean = []
                     break  # skip this user
-            if user_id not in user_ids and favourites_clean != []:
-                user_list.append({"profile": user_id, "favourite_anime": favourites_clean})
-                user_ids.add(user_id)
-    return user_list
+            if user_id not in user_map and len(favourites_clean) >= USER_MINIMUM_FAVOURITES:
+                user_map[user_id] = favourites_clean
+    return user_map
+
+
+def get_anime_id_title_map(anime_file: str, user_map: dict[str, list[int]]) -> dict[int, str]:
+    """
+    Return a dictionary mapping an anime integer id to its string title. It skips duplicates of an anime and filters out
+    anime which are not in a list of favourite from the users
+    """
+    anime_ids_from_user = set()
+    for username in user_map:
+        for anime in user_map[username]:
+            anime_ids_from_user.add(anime)
+
+    df = pd.read_csv(anime_file)
+    anime_map = {}
+    for _, row in df.iterrows():
+        anime_id = int(row['uid'])
+        title = row['title']
+        if anime_id not in anime_map and anime_id in anime_ids_from_user:
+            anime_map[anime_id] = title
+    return anime_map
+
+
+def remove_under_favourites_lower_bound(list_of_users: dict[str, list[int]], limit: int) -> dict[str, list[int]]:
+    """
+    Given a list of user dictionaries, return the list with only those that have a number of favourite anime above the
+    inputted integer.
+    """
+    new_users = {}
+    for user in list_of_users:
+        if len(list_of_users[user]) >= limit:
+            new_users[user] = list_of_users[user]
+    return new_users
 
 
 if __name__ == "__main__":
-    print(clean_profiles("data/profiles_small.csv"))
+    user_mapping = clean_profiles("data/profiles_small.csv")
+    print(user_mapping)
+    print(get_anime_id_title_map("data/animes_small.csv", user_mapping))
