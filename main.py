edf48982daf2dccc507467bec33fa3bf6e330164
Miray-la <mgunesv8@gmail.com>
2026-03-30 18:53:29 -0400

    new function added - call_graphs_and_transform -     this function is calling the user_graph and attribute_graph, passes the outcome to combined_recommendation function

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import user_rating_graph
 import attribute_graph
 
 
-def combined_recommendation(user_recs: dict[str, float], attribute_recs: dict[str, float], returned_limit) -> list[str]:
+def combined_recommendation(user_recs: dict[str, int], attribute_recs: dict[str, int], returned_limit) -> list[str]:
     """
     Return a final list of anime titles based on the user-based recommendation and attribute-based recommendation and
     combining them. The final list has a chosen number returned.
@@ -28,9 +28,27 @@ def combined_recommendation(user_recs: dict[str, float], attribute_recs: dict[st
     return [anime for anime, score in sorted_anime[:returned_limit]]
 
 
-if __name__ == "__main__":
+def call_graphs_and_transform(attribute_graph, user_rating_graph,
+                               inputted_ratings: dict[str, int], limit: int, data: str) ->list[str]:
+    """
+    this function is calling the user_graph and attribute_graph, passes the outcome to combined_recommendation function
+    """
+    shows_user = list(inputted_ratings.keys())  # we are extracting titles given by user
 
+    attribute_based_graph = attribute_graph.recommend_new_show(shows_user, limit)
+    user_based_graph = user_rating_graph.recommend_anime(inputted_ratings, limit)
+    #we got the output lists of each graph
+
+    return combined_recommendation(user_based_graph, attribute_based_graph, limit)
+
+
+if __name__ == "__main__":
     # g = user_graph.load_user_graph("data/profiles.csv", "data/animes.csv")
+    # print(g.get_all_vertices())
     # print(g.recommend_anime(['Haikyuu!! Second Season', 'Shigatsu wa Kimi no Uso', 'Made in Abyss', 'Fullmetal Alchemist: Brotherhood', 'Kizumonogatari III: Reiketsu-hen'], 20, 50))
-    wg = user_rating_graph.load_user_graph("data/reviews.csv", "data/animes.csv")
-    print(wg.recommend_anime_weighted({"Fullmetal Alchemist: Brotherhood": 10, "Steins;Gate": 9, "Shingeki no Kyojin": 8, "Death Note": 7, "One Punch Man": 6 }, 100, 100))
+    wg = user_rating_graph.load_user_graph("reviews_small.csv", "animes.csv")
+    ag = attribute_graph.load_the_graph("animes.csv")
+
+    print(wg.recommend_anime({'Haikyuu!! Second Season': 5, 'Shigatsu wa Kimi no Uso': 7, 'Made in Abyss': 6}, 15))
+    # print(wg.get_weight("OVERPOWERED99", "One Punch Man"))
+    # print(wg.get_neighbours("One Punch Man"))
